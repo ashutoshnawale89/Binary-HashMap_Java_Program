5cdc043e8bfda43b7b56d9c16cf944bbbc61578c
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-14 10:34:22 +0530

    Refactor: Search the Number In Binary Tree

--- src/com/program/BinarySearchTree.java
+++ src/com/program/BinarySearchTree.java
@@ -34,15 +34,34 @@ public class BinarySearchTree {
 	    preorder(root.left);
 	    preorder(root.right);
 	 }
-	   
+	 public boolean ifNodeExists( Node root, int input)
+	    {
+	        if (root == null)
+	            return false;
+	     
+	        if (root.data == input)
+	            return true;
+	     
+	        // then recur on left subtree /
+	        boolean res1 = ifNodeExists(root.left, input);
+	       
+	        // node found, no need to look further
+	        if(res1==true) return true;
+	     
+	        // node is not found in left,
+	        // so recur on right subtree /
+	        boolean res2 = ifNodeExists(root.right, input);
+	     
+	        return res2;
+	    }
 	   public static void main(String args[]) {
 	       int nodes[] = {56, 30,22,11,3,-1,-1,16,-1,-1,-1,40,-1,
 	    		   -1,70,60,-1,65,62,-1,-1,67,-1,-1,95,-1,-1};
+	       
 	       BinarySearchTree tree = new BinarySearchTree();  
 	       Node root = tree.buildTree(nodes);
-	       tree.preorder(root);
-	       System.out.println("  ");
-	       System.out.println("The Value Of  "+-1 +" ------> Will Indicate Null");
+	       //   Search The Tree Input are In Below  put
+	       System.out.println(tree.ifNodeExists(root, 67));
 	   }
 }
 
